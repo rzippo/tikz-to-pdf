d5b500a2431e8bc8f1fac5062cac5ca92f5b5551
Raffaele Zippo <raffaele.zippo@outlook.it>
2023-06-25 11:24:51 +0200

    Add install script

---
+++ install.ps1
@@ -0,0 +1,20 @@
+# Build latest code
+dotnet publish -c Release .\tikz-to-pdf\tikz-to-pdf.csproj
+
+# Create install folder if not exists, else clear its contents
+$installFolder = "$env:LOCALAPPDATA\Programs\tikz-to-pdf";
+if( -not (Test-Path $installFolder) ) {
+    New-Item $installFolder -ItemType Directory
+}
+else {
+    Remove-Item -Recurse "$installFolder\*"
+}
+
+# Copy build to install folder
+Copy-Item -Recurse .\tikz-to-pdf\bin\Release\net7.0\publish\* $env:LOCALAPPDATA\Programs\tikz-to-pdf
+
+# If install folder is not in path, add it
+if(-not ($env:Path -contains $installFolder)) {
+    $env:Path += ";$installFolder"
+    [System.Environment]::SetEnvironmentVariable("Path", $env:Path, [EnvironmentVariableTarget]::User)
+}
